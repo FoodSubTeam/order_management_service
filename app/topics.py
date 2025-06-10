0d2979c62c968b9973241f6721269ca602868157
Aleksandra Kasp <olala10@users.noreply.github.com>
2025-06-10 21:30:24 +0200

    Update topics.py in order_management_service

--- app/topics.py
+++ app/topics.py
@@ -4,6 +4,7 @@ class Topic(Enum):
     OFFER_SELECTED = "offer.selected"
     SUBSCRIPTION_PAID = "subscription.paid"
     KITCHEN_ORDER = "kitchen.order"
+    DELIVERY_ORDER = "delivery.order"
 
 
 class MessageType(Enum):
@@ -12,3 +13,4 @@ class MessageType(Enum):
     OFFER_PAYMENT_FAILURE = "offer_payment_failure"
     MARK_SUBSCRIPTION_AS_PAYED = "mark_subscription_as_paid"
     GENERATE_KITCHEN_ORDERS = "generate_kitchen_orders"
+    GENERATE_DELIVERY_ORDERS = "generate_delivery_orders"
